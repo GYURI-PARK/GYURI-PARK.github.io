3c9aa6b77502dee844771410f10abddd874960f8
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2022-12-23 15:48:41 +0900

    wayfinder

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -79,15 +79,14 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: '사용자 맞춤형 길 찾기 서비스 WayFindeR 개발',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+          '“여러분은 목적지까지의 길을 찾을 때 어떻게 하시나요?” 기존의 우리가 사용하고 있는 길 찾기 서비스들은 보행환경을 고려하지 않은 채, 최단거리 / 최소 시간 위주의 경로만을 제공합니다. 단순히 빨리 갈 수 있는 길이 아닌, 안전하고 쾌적한 내가 걷고 싶은 길을 제공해 주는 서비스가 있다면 어떨까요? 안전성, 쾌적성 기반의 보행자 맞춤형 길 찾기 서비스, WayFindeR입니다.',
+        techStack: ['HTML', 'CSS','JavaScript','Node.js'],
+        thumbnailUrl: '맞춤형서비스.jpeg',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: 'https://uos-urbanscience.org/archives/uos_portfolio/당신이-원하는-길로-이끄는-지도-wayfinder',
+          github: 'https://github.com/GYURI-PARK/2022_Graduation_project',
         },
       },
     ],
